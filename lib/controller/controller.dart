1fec622ae4be6be01cad47063069e0ecf9ecad86
ngohieuphat <ngohipha1@gmail.com>
2022-07-11 09:47:15 +0700

    controller

---
+++ lib/controller/controller.dart
@@ -0,0 +1,27 @@
+import 'package:banhang/repository/repository.dart';
+import 'package:get/get.dart';
+
+//su ly api 
+class Controller extends GetxController {
+  var isLoading = true.obs;
+  var groceryList = [].obs;
+
+  @override
+  void onInit() {
+    fetch();
+    super.onInit();
+  }
+
+  void fetch() async {
+    try {
+      isLoading(true);
+      var groceries = (await Repo.getData())?.data!.products.results;
+      if (groceries != null) {
+        //rx khoi tao gt
+        Rx(groceryList.value = groceries);
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
